977aac86ae16bbafa2d49450f41b159eb7de01b6
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-05-28 02:03:57 +0300

    Remove unused food plural name

--- app/Food.php
+++ app/Food.php
@@ -8,14 +8,12 @@ use Nette\Schema\Expect;
 class Food
 {
     private string $name;
-    private string $namePlural;
     private int $price;
     private float $nutritionalRatio;
 
     public function __construct(\stdClass $properties)
     {
         $this->name = $properties->name;
-        $this->namePlural = $properties->namePlural;
         $this->price = $properties->price;
         $this->nutritionalRatio = $properties->nutritionalRatio;
     }
@@ -24,7 +22,6 @@ class Food
     {
         return Expect::structure([
             "name" => Expect::string(),
-            "namePlural" => Expect::string(),
             "price" => Expect::int(100),
             "nutritionalRatio" => Expect::type("int|float")->default(1),
         ]);
